0f13967153338138aac694bbac51094329a1fb83
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-11-30 08:46:30 +0700

    feat: implement SkorMessage class and update level UI text

---
+++ Assets/SkorMessage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkorMessage : MonoBehaviour
+{
+    public Text pesanText;
+
+    void Start()
+    {
+        int skor = PlayerPrefs.GetInt("skor");
+
+        if (skor >= 70)
+        {
+            pesanText.text = "Selamat! Kamu mendapatkan skor tinggi!";
+        }
+        else
+        {
+            pesanText.text = "Tetap semangat! Kamu bisa mencoba lagi.";
+        }
+    }
+}
